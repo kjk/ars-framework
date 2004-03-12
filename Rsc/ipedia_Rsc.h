9add9588d7977db6a21a15e746ae4c87f333a0a4
andrzejc <andrzejc@10a9aba9-86da-0310-ac04-a2df2cc00fd9>
2004-03-12 11:22:35 +0000

    added def. too big alert
    git-svn-id: https://repo.kowalczyk.info/svn/ars_framework/trunk/ipedia@51 10a9aba9-86da-0310-ac04-a2df2cc00fd9

--- Rsc/ipedia_Rsc.h
+++ Rsc/ipedia_Rsc.h
@@ -1,10 +1,11 @@
 /* pilrc generated file.  Do not edit!*/
-#define termInputField 1011
-#define termLabel 1010
-#define definitionScrollBar 1009
-#define goButton 1008
-#define mainForm 1007
-#define notEnoughMemoryAlert 1006
+#define termInputField 1012
+#define termLabel 1011
+#define definitionScrollBar 1010
+#define goButton 1009
+#define mainForm 1008
+#define notEnoughMemoryAlert 1007
+#define definitionTooBigAlert 1006
 #define definitionNotFoundAlert 1005
 #define networkUnavailableAlert 1004
 #define romIncompatibleAlert 1003
