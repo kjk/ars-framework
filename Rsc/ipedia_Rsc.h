3c3016c4c812a8b6ce2eaea7575d5f101590579f
andrzejc <andrzejc@10a9aba9-86da-0310-ac04-a2df2cc00fd9>
2004-03-16 18:47:55 +0000

    added some error alerts
    git-svn-id: https://repo.kowalczyk.info/svn/ars_framework/trunk/ipedia@68 10a9aba9-86da-0310-ac04-a2df2cc00fd9

--- Rsc/ipedia_Rsc.h
+++ Rsc/ipedia_Rsc.h
@@ -1,12 +1,17 @@
 /* pilrc generated file.  Do not edit!*/
-#define currentTermField 1017
-#define forwardButton 1016
-#define backButton 1015
-#define termInputField 1014
-#define termLabel 1013
-#define definitionScrollBar 1012
-#define goButton 1011
-#define mainForm 1010
+#define currentTermField 1022
+#define forwardButton 1021
+#define backButton 1020
+#define termInputField 1019
+#define termLabel 1018
+#define definitionScrollBar 1017
+#define goButton 1016
+#define mainForm 1015
+#define malformedResponseAlert 1014
+#define malformedRequestAlert 1013
+#define unsupportedDeviceAlert 1012
+#define invalidAuthorizationAlert 1011
+#define serverFailureAlert 1010
 #define notEnoughMemoryAlert 1009
 #define definitionTooBigAlert 1008
 #define definitionNotFoundAlert 1007
