5e6ec2a33f77fb59a82e73643dd726f2d3599cea
kjk <kjk@10a9aba9-86da-0310-ac04-a2df2cc00fd9>
2004-04-06 01:41:21 +0000

    add ability to split the output into multiple files
    git-svn-id: https://repo.kowalczyk.info/svn/ars_framework/trunk/ipedia@137 10a9aba9-86da-0310-ac04-a2df2cc00fd9

--- Server/dumpEnwikiToFile.py
+++ Server/dumpEnwikiToFile.py
@@ -5,12 +5,20 @@
 # Only dump the title (prepended with ^ for easy browsing in
 # text editor) and the text
 #
-# Todo:
-#  * add ability to dump the original and converted to 2 files
-#    so that we can use a diffing tool to compare them and see
-#    changes (e.g. using -orig orig_dump.txt -converted converted_dump.txt)
-#    Those should be good stress tool for diffing tools.
-import MySQLdb, sys, datetime, re, unicodedata, converter
+# Usage:
+#  -limit N : limits the number of articles to convert
+#
+#  If no more arguments given, will dump the data to stdout
+#
+#  -orig fielName : name of the file to which write the original data
+#  -converted fileName : name of the file to which write converted data
+#  -split N : if given, write to N files, naming them $BASE-nn.$EXT e.g.
+#             if "-orig foo.txt" and N is 3, write to 3 files "foo-01.txt",
+#             "foo-02.txt" and "foo-03.txt".
+#             The idea is that we want to compare those files using windiff or
+#             some other diffing program but they don't handle the files as big
+#             as we produce (>300 MB)
+import sys, string, re, datetime, unicodedata, MySQLdb, converter
 
 g_conn        = None
 
@@ -103,23 +111,59 @@ def dumpAll(articleLimit):
             break
         curOffset += rowsPerQuery
 
+def genSplitFileName(nameBase, num):
+    if None == num:
+        return nameBase
+    (name,ext) = string.split(nameBase,".")
+    result = "%s-%02d.%s" % (name,num,ext)
+    #print "result: %s" % result
+    return result
+
+def testGenSplitFileName():
+    assert genSplitFileName("test.txt",None) == "test.txt"
+    assert genSplitFileName("test.txt",1) == "test-01.txt"
+    assert genSplitFileName("foo.bar",11) == "foo-11.bar"
+
+g_foOrig = None
+g_foConverted = None
+def createFiles(fileOrig,fileConverted,num):
+    global g_foOrig, g_foConverted
+    g_foOrig = open( genSplitFileName(fileOrig,num), "wb")
+    g_foConverted = open( genSplitFileName(fileConverted,num), "wb")
+
+def closeFiles():
+    global g_foOrig, g_foConverted
+    g_foOrig.close()
+    g_foConverted.close()
+
+
 # dump all articles to a file. if articleLimit != None
 # limit the number of articles to dump
-def dumpAllWithConverted(articleLimit,fileOrig,fileConverted):
-    global g_conn
+# fileOrig and fileConverted are the names of files where to write original
+# and converted content of the enwiki database
+# if splitPartsCount is give, write to that many files
+def dumpAllWithConverted(articleLimit,fileOrig,fileConverted,splitPartsCount):
+    global g_conn, g_foOrig, g_foConverted
 
-    foOrig = open(fileOrig, "wb")
-    foConverted = open(fileConverted, "wb")
+    assert fileOrig
+    assert fileConverted
 
     rowCount = 0
     rowsLeft = getEnwikiRowCount()
+    if articleLimit and rowsLeft > articleLimit:
+        rowsLeft = articleLimit
+    curPartNo = None
+    splitAfter = None
+    if splitPartsCount:
+        splitAfter = rowsLeft / splitPartsCount
+        curPartNo = 1
+
+    createFiles(fileOrig, fileConverted, curPartNo)
     rowsPerQuery = 2500
     curOffset = 0
     cursor = getNamedCursor(g_conn,"all_cur_ids")
     while True:
         query="""SELECT cur_title,cur_text,cur_timestamp FROM cur WHERE cur_namespace=0"""
-        if len(sys.argv)>1:
-            query+=""" AND cur_timestamp>'%s'""" % dbEscape(sys.argv[1])
         query+=""" LIMIT %d,%d""" % (curOffset, rowsPerQuery)
         #print query
         cursor.execute(query)
@@ -127,14 +171,14 @@ def dumpAllWithConverted(articleLimit,fileOrig,fileConverted):
         for row in cursor.fetchall():
             title = "^" + row[0] + "\n"
             cur_txt = row[1]
-            foOrig.write(title)
-            foConverted.write(title)
+            g_foOrig.write(title)
+            g_foConverted.write(title)
 
-            foOrig.write(cur_txt)
-            foOrig.write("\n")
+            g_foOrig.write(cur_txt)
+            g_foOrig.write("\n")
             converted = converter.convertDefinition(cur_txt)
-            foConverted.write(converted)
-            foConverted.write("\n")
+            g_foConverted.write(converted)
+            g_foConverted.write("\n")
             rowCount += 1
             rowsLeft -= 1
             processedInOneFetch += 1
@@ -143,11 +187,14 @@ def dumpAllWithConverted(articleLimit,fileOrig,fileConverted):
             if articleLimit and rowCount >= articleLimit:
                 processedInOneFetch # a hack so that we exit the loop but not by doing return
                 break
+            if splitAfter and (0 == rowCount % splitAfter):
+                closeFiles()
+                curPartNo += 1
+                createFiles(fileOrig,fileConverted,curPartNo)
         if 0 == processedInOneFetch:
             break
         curOffset += rowsPerQuery
-    foOrig.close()
-    foConverted.close()
+    closeFiles()
 
 def fDetectRemoveCmdFlag(flag):
     fFlagPresent = False
@@ -174,14 +221,24 @@ def getRemoveCmdArg(argName):
 
 if __name__=="__main__":
 
+    testGenSplitFileName()
     initDatabase()
     limit = getRemoveCmdArg("-limit")
     if limit:
         limit = int(limit)
     origFile = getRemoveCmdArg("-orig")
     convertedFile = getRemoveCmdArg("-converted")
+    splitPartsCount = getRemoveCmdArg("-split")
+    if splitPartsCount:
+        splitPartsCount = int(splitPartsCount)
+        if not origFile or not convertedFile:
+            print "Usage: if -split given, must also give -orig and -converted"
+            sys.exit(0)
+    if len(sys.argv) > 1:
+        print "Usage: dumpEnfikiToFile.py [-limit N] [-orig fileName] [-converted fileName] [-split N]"
+        sys.exit(0)
     if origFile and convertedFile:
-        dumpAllWithConverted(limit,origFile,convertedFile)
+        dumpAllWithConverted(limit,origFile,convertedFile,splitPartsCount)
     else:
          dumpAll(limit)
     deinitDatabase()
