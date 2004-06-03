dba0eb3383787f415f3b870ad9efc602c3fdbdc8
mareks <mareks@10a9aba9-86da-0310-ac04-a2df2cc00fd9>
2004-06-03 22:48:30 +0000

    Conversion to Windows CE.
    git-svn-id: https://repo.kowalczyk.info/svn/ars_framework/trunk/ars_framework@337 10a9aba9-86da-0310-ac04-a2df2cc00fd9

---
+++ Src/SocketAddress.cpp
@@ -0,0 +1,30 @@
+#include <SocketAddress.hpp>
+#if !(defined(_WIN32_WCE) || defined(_WIN32))
+#include <cstring>
+#endif
+
+namespace ArsLexis
+
+{
+
+    SocketAddress::SocketAddress()
+    {
+        using namespace std;
+        memset(&address_, sizeof(address_), 0);
+    }
+
+    INetSocketAddress::INetSocketAddress(const IPAddr& addr, ushort_t port, short addressFamily)
+    {
+        address().family=addressFamily;
+        address().port=netToHostS(port);
+        address().ip=addr;
+    }
+    
+    INetSocketAddress::INetSocketAddress()
+    {
+        address().family=socketAddrINET;
+        address().port=0;
+        address().ip.ip=0;
+    }
+    
+}
