9af8ba67135032d2f6cdd55cd86788e108fc8cee
szymonk <szymonk@10a9aba9-86da-0310-ac04-a2df2cc00fd9>
2004-06-11 11:05:39 +0000

    KxmlParser - completlyLoose
    git-svn-id: https://repo.kowalczyk.info/svn/ars_framework/trunk/ars_framework@361 10a9aba9-86da-0310-ac04-a2df2cc00fd9

--- Src/KXmlParser/KXmlParser.cpp
+++ Src/KXmlParser/KXmlParser.cpp
@@ -319,6 +319,7 @@ error_t KXmlParser::readName(String& ret)
     error_t error;
     int pos = txtPos_;
     int c;
+    bool weAreInsiddeQuote = false;
     if ((error=peek(c,0))!=eNoError)
         return error;
 
@@ -326,11 +327,14 @@ error_t KXmlParser::readName(String& ret)
         && (c < _T('A') || c > _T('Z'))
         && c != _T('_')
         && c != _T(':')
+        && !(completlyLoose_ && c == _T('\"'))
         && c < 0x0c0)
         return eNameExpected;
 
     do
     {
+        if(c == _T('\"'))
+            weAreInsiddeQuote = !weAreInsiddeQuote;
         if ((error=read(c))!=eNoError)
             return error;
         push(c);
@@ -340,6 +344,7 @@ error_t KXmlParser::readName(String& ret)
         || (c >= _T('A') && c <= _T('Z'))
         || (c >= _T('0') && c <= _T('9'))
         || (completlyLoose_ && c == _T('\"'))
+        || (weAreInsiddeQuote && completlyLoose_ && c == _T('/'))
         || c == _T('_')
         || c == _T('-')
         || c == _T(':')
