8cbecd7a73a9a44f3cf5914422a95392cb8622cd
kjk <kjk@10a9aba9-86da-0310-ac04-a2df2cc00fd9>
2004-08-08 09:44:56 +0000

    disable font caching - doesn't seem to work in iNoah
    git-svn-id: https://repo.kowalczyk.info/svn/ars_framework/trunk/ars_framework@508 10a9aba9-86da-0310-ac04-a2df2cc00fd9

--- Src/WindowsCE/WinFont.cpp
+++ Src/WindowsCE/WinFont.cpp
@@ -1,39 +1,51 @@
 #include <BaseTypes.hpp>
+#include <Debug.hpp>
+
 #include "WinFont.h"
 
 using namespace ArsLexis;
+
+typedef struct FontCacheEntry_ {
+    HFONT fntHandle;
+
+} FontCacheEntry_t;
 
 // cache of font handles
 HFONT g_fontHandles[16-6][3][2][2];
 
 static void InitFontHandles()
 {
-	static bool g_fInitialized = false;
-	if (g_fInitialized)
-		return;
+    static bool g_fInitialized = false;
+    if (g_fInitialized)
+        return;
 
-	for (int a=0; a<16-6; a++)
-	{
-		for (int b=0; b<3; b++)
-		{
-			for (int c=0; c<2; c++)
-			{
-				for (int d=0; d<2; d++)
-				{
-					g_fontHandles[a][b][c][d];
-				}
-			}
-		}
-	}
-	g_fInitialized = true;
+    for (int a=0; a<16-6; a++)
+    {
+        for (int b=0; b<3; b++)
+        {
+            for (int c=0; c<2; c++)
+            {
+                for (int d=0; d<2; d++)
+                {
+                    g_fontHandles[a][b][c][d];
+                }
+            }
+        }
+    }
+    g_fInitialized = true;
 }
 
 static HFONT createFont(int height, int weight, int realWeight, int isUnder, int isStrike)
 {
-	InitFontHandles();
+    // InitFontHandles();
 
-    if (NULL != g_fontHandles[height-6][weight][isUnder][isStrike])
-        return g_fontHandles[height-6][weight][isUnder][isStrike];
+    assert( (height-6>=0) && (height-6<16-6) );
+    assert( (weight>=0) && (weight<3) );
+    assert( (isUnder>=0) && (isUnder<2) );
+    assert( (isStrike>=0) && (isStrike<2) );
+
+/*    if (NULL != g_fontHandles[height-6][weight][isUnder][isStrike])
+        return g_fontHandles[height-6][weight][isUnder][isStrike];*/
 
     LOGFONT logfnt;
     HFONT   stdFont = (HFONT)GetStockObject(SYSTEM_FONT);
@@ -49,12 +61,18 @@ static HFONT createFont(int height, int weight, int realWeight, int isUnder, int
     assert(height<maxFontHeight);
 
     HFONT newFont = CreateFontIndirect(&logfnt);
-    if (NULL == newFont)
+
+    if (newFont)
+        return newFont;
+    else
+        return stdFont;
+
+/*    if (NULL == newFont)
         g_fontHandles[height-6][weight][isUnder][isStrike] = stdFont;
     else
         g_fontHandles[height-6][weight][isUnder][isStrike] = newFont;
 
-    return g_fontHandles[height-6][weight][isUnder][isStrike];
+    return g_fontHandles[height-6][weight][isUnder][isStrike]; */
 }
 
 HFONT getFont(int height, FontEffects effects)
