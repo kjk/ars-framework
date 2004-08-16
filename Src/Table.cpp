78d86748044b573a08a852c0df716cd9790c098c
andrzejc <andrzejc@10a9aba9-86da-0310-ac04-a2df2cc00fd9>
2004-08-16 20:42:39 +0000

    added class Table - interface to PalmOS' table UI object
    git-svn-id: https://repo.kowalczyk.info/svn/ars_framework/trunk/ars_framework@533 10a9aba9-86da-0310-ac04-a2df2cc00fd9

---
+++ Src/Table.cpp
@@ -0,0 +1,5 @@
+#include <Table.hpp>
+
+using namespace ArsLexis;
+
+Table::~Table() {}
---
+++ Src/Table.hpp
@@ -0,0 +1,65 @@
+#ifndef ARSLEXIS_TABLE_HPP__
+#define ARSLEXIS_TABLE_HPP__
+
+#include <FormObject.hpp>
+
+namespace ArsLexis {
+
+    class Table: public FormObjectWrapper<TableType> {
+    public:
+    
+        explicit Table(Form& form, UInt16 id=frmInvalidObjectId):
+            FormObjectWrapper(form, id)
+        {}
+        
+        ~Table();
+        
+        void redraw() {TblRedrawTable(object());}
+        void invalidate() {TblMarkTableInvalid(object());}
+        
+        void invalidateRow(UInt16 row) {TblMarkRowInvalid(object(), row);}
+
+        bool isRowInvalid(UInt16 row) const {return TblRowInvalid(object(), row);}
+        
+        bool isBeingEdited() const {return TblEditing(object());}
+        
+        void erase() {TblEraseTable(object());}
+        
+        Coord columnSpacing(UInt16 column) const {return TblGetColumnSpacing(object(), column);}
+        
+        void setColumnSpacing(UInt16 column, Coord spacing) {TblSetColumnSpacing(object(), column, spacing);}
+        
+        Coord columnWidth(UInt16 column) const {return TblGetColumnWidth(object(), column);}
+        
+        void setColumnWidth(UInt16 column, Coord width) {TblSetColumnWidth(object(), column, width);}
+        
+        UInt16 columnsCount() const {return TblGetNumberOfColumns(object());}
+        
+        UInt16 rowsCount() const {return TblGetNumberOfRows(object());}
+        
+        Coord rowHeight(UInt16 row) const {return TblGetRowHeight(object(), row);}
+        
+        void insertRow(UInt16 row) {TblInsertRow(object(), row);}
+        
+        void removeRow(UInt16 row) {TblRemoveRow(object(), row);}
+        
+        void setRowUsable(UInt16 row, bool usable) {TblSetRowUsable(object(), row, usable);}
+        
+        bool isRowUsable(UInt16 row) const {return TblRowUsable(object(), row);}
+        
+        void setItemStyle(UInt16 row, UInt16 column, TableItemStyleType style) {TblSetItemStyle(object(), row, column, style);}
+        
+        void setItemInt(UInt16 row, UInt16 column, Int16 value) {TblSetItemInt(object(), row, column, value);}
+        
+        void setItemPtr(UInt16 row, UInt16 column, void* ptr) {TblSetItemPtr(object(), row, column, ptr);}
+        
+        Int16 itemInt(UInt16 row, UInt16 column) const {return TblGetItemInt(object(), row, column);}
+        
+        void* itemPtr(UInt16 row, UInt16 column) const {return TblGetItemPtr(object(), row, column);}
+        
+        
+    };
+    
+}
+
+#endif
