7d85bbac0fb97113b698294c56d1cb2362d6caf1
kjk <kjk@10a9aba9-86da-0310-ac04-a2df2cc00fd9>
2004-09-04 10:31:31 +0000

    implement calling phone number on Treo
    git-svn-id: https://repo.kowalczyk.info/svn/ars_framework/trunk/ars_framework@577 10a9aba9-86da-0310-ac04-a2df2cc00fd9

--- Src/PhoneCall.cpp
+++ Src/PhoneCall.cpp
@@ -1,49 +1,91 @@
 #include <PhoneCall.hpp>
 
-//#include <PhoneLib.h>
+#include <TelephonyMgr.h>
+#include <TelephonyMgrTypes.h>
 
-namespace ArsLexis
+#include <HsExt.h>
+#include <HsAppLaunchCmd.h>
+
+#define MAX_PHONE_NUMBER 32
+
+// this is a text that shows up in the "Phone" application
+#define CALLER_NAME "InfoMan"
+
+// dial a given number by invoking Treo 600 built-in phone application
+// this works on Treo 600 but according to http://news.palmos.com/read/messages?id=162343#162343
+// it's better to use HelperServiceClass (works on more devices)
+// Phone app accepts phone numbers in variety of forms e.g.
+// xxx-yyyy-zzzz, (xxx) yyy-zzzz
+Err DialPhoneNumber(const char *phoneNumber)
 {
-/*
-    static Err LoadPhoneLibrary(UInt16* libRefP, Boolean* libLoadedP)
-    {
-        Err error = 0;
-        // Routine is pointless without this parameter
-        if (!libRefP)
-            return memErrInvalidParam;
-        // Get the Phone library
-        error = SysLibFind("GSM Library", libRefP);
-        if (error)
-        {
-        
-        assert( 0 == sysErrLibNotFound);
-        assert( 0 == sysErrNoFreeRAM);
-        assert( 0 == sysErrNoFreeLibSlots);        
-            // It's not already here - have to load it ourselves
-            error = SysLibLoad('libr', 'GSM!', libRefP);
-            if (error)
-                return error;
-            if (libLoadedP)
-                *libLoadedP = true;
-        }
-        return error;
-    }
+    UInt16      cardNo;
+    LocalID     dbID;
+    DmSearchStateType SearchState;
+    Err err = DmGetNextDatabaseByTypeCreator(true, &SearchState, sysFileTApplication, hsFileCPhone, true, &cardNo, &dbID);
+    if (err)
+        return err;
 
-*/
-    bool call10DigitsNumber(const ArsLexis::String& callNumber)
-    {
-/*        UInt16 libRefP = 0;
-        Boolean libLoadedP = true;
+    int callerNameLen = sizeof(CALLER_NAME)-1;  // sizeof() includes terminating 0
+    int dataLen = sizeof(PhoneAppLaunchCmdDialType)+MAX_PHONE_NUMBER+1+callerNameLen+1;
+    char *dataPtr = (char*)MemPtrNew(dataLen);
+    if (NULL==dataPtr) 
+        return sysErrNoFreeRAM;
+    MemSet(dataPtr, dataLen, 0);
 
-        Err error = LoadPhoneLibrary(&libRefP, &libLoadedP);
-        if (error)
-            return false;
-    
-        PhnLibPlayDTMF(libRefP,(char*)callNumber.c_str());
-    
-        PhnLibClose (libRefP);
-        return true;
-*/        
-        return false;
-    }
+    PhoneAppLaunchCmdDialPtr dialInfo;
+    dialInfo = (PhoneAppLaunchCmdDialPtr)dataPtr;
+
+    char *number = dataPtr+sizeof(PhoneAppLaunchCmdDialType);
+    char *callerName = number+MAX_PHONE_NUMBER+1;
+
+    MemMove(number, phoneNumber, StrLen(phoneNumber));
+    MemMove(callerName, CALLER_NAME, callerNameLen);
+    dialInfo->number = number;
+    dialInfo->name = callerName;
+    dialInfo->version = 1;
+    dialInfo->dialMethod = PhoneAppDialMethodNormal;
+    MemPtrSetOwner(dataPtr, 0); /* owned by system */
+    err = SysUIAppSwitch(cardNo, dbID, phoneAppLaunchCmdDial, dataPtr);
+    return err;
 }
+
+
+// this doesn't work on Treo
+bool FCallPhoneNumber(const char* phoneNumber)
+{
+    UInt16    gTelRefNum = 0;
+    TelAppID  gTelAppID;
+    Err       err;
+    bool      fOk = false;
+
+    err = SysLibFind(kTelMgrLibName, &gTelRefNum);
+    if (errNone != err)
+    {
+        /* The library is not loaded, so load it. */
+        err = SysLibLoad(kTelMgrDatabaseType, kTelMgrDatabaseCreator, &gTelRefNum);
+        if (err)
+            goto Exit;
+    }
+
+    err = TelOpen(gTelRefNum, kTelMgrVersion, &gTelAppID);
+    if (errNone != err)
+        goto Exit;
+
+    err = TelIsSpcCallNumberSupported(gTelRefNum, gTelAppID, NULL);
+    if (errNone != err)
+        goto CloseAndExit;
+
+    err = TelSpcCallNumber(gTelRefNum, gTelAppID, phoneNumber, NULL);
+    if (errNone != err)
+        goto CloseAndExit;
+
+    fOk = true;
+
+CloseAndExit:
+    if ( telErrLibStillInUse != TelClose(gTelRefNum, gTelAppID))
+        SysLibRemove(gTelRefNum);
+
+Exit:
+    return fOk;
+}
+
--- Src/PhoneCall.hpp
+++ Src/PhoneCall.hpp
@@ -1,12 +1,7 @@
 #ifndef __ARSLEXIS_PHONE_CALL_HPP__
 #define __ARSLEXIS_PHONE_CALL_HPP__
 
-#include <Debug.hpp>
-#include <BaseTypes.hpp>
-
-namespace ArsLexis 
-{
-    bool call10DigitsNumber(const ArsLexis::String& callNumber);
-}
+Err DialPhoneNumber(const char *phoneNumber);
+bool FCallPhoneNumber(const char *phoneNumber);
 
 #endif
