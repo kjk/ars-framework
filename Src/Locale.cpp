3d777cbe405ab0d7ed7741d2a9148ae560518d45
kjk <kjk@10a9aba9-86da-0310-ac04-a2df2cc00fd9>
2004-09-19 18:09:53 +0000

    add more languages
    git-svn-id: https://repo.kowalczyk.info/svn/ars_framework/trunk/ars_framework@600 10a9aba9-86da-0310-ac04-a2df2cc00fd9

--- Src/Locale.cpp
+++ Src/Locale.cpp
@@ -4,7 +4,9 @@
 
 using namespace ArsLexis;
 
-//! @todo as number of languages grows, switch to sorted array and binary search
+// Note: it has more languages than we support on the server currently.
+// This is for backwards compatibility. We want to make sure that current
+// client will also handle future languages when we add them on the server.
 const char_t* ArsLexis::GetLangNameByLangCode(const String& langCode)
 {
     if (langCode.empty() || _T("en") == langCode)
@@ -18,6 +20,62 @@ const char_t* ArsLexis::GetLangNameByLangCode(const String& langCode)
     else if (_T("de") == langCode)        
     {
         return _T("German");
+    }
+    else if (_T("es") == langCode)
+    {
+        return _T("Spanish");
+    }
+    else if (_T("it") == langCode)
+    {
+        return _T("Italian");
+    }
+    else if (_T("ja") == langCode)
+    {
+        return _T("Japanese");
+    }
+    else if (_T("nl") == langCode)
+    {
+        return _T("Dutch");
+    }
+    else if (_T("pl") == langCode)
+    {
+        return _T("Polish");
+    }
+    else if (_T("pt") == langCode)
+    {
+        return _T("Portuguese");
+    }
+    else if (_T("ru") == langCode)
+    {
+        return _T("Russian");
+    }
+    else if (_T("sv") == langCode)
+    {
+        return _T("Swedish");
+    }
+    else if (_T("zh") == langCode)
+    {
+        return _T("Chinese");
+    }
+    else if (_T("da") == langCode)
+    {
+        return _T("Danish");
+    }
+    else if (_T("eo") == langCode)
+    {
+        return _T("Esperanto");
+    }
+    else if (_T("he") == langCode)
+    {
+        return _T("Hebrew");
+    }
+    else if (_T("hu") == langCode)
+    {
+        return _T("Hungarian");
+    }
+    else if (_T("no") == langCode)
+    {
+        return _T("Norwegian");
     }
     else
     {
