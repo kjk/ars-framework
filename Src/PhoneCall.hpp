f82c391b5741627dd1f6ee18970ecec67f694800
kjk <kjk@10a9aba9-86da-0310-ac04-a2df2cc00fd9>
2005-02-02 09:14:24 +0000

    make it compile even without pre-compiled headers
    git-svn-id: https://repo.kowalczyk.info/svn/ars_framework/trunk/ars_framework@758 10a9aba9-86da-0310-ac04-a2df2cc00fd9

--- Src/PhoneCall.hpp
+++ Src/PhoneCall.hpp
@@ -1,6 +1,8 @@
 #ifndef __ARSLEXIS_PHONE_CALL_HPP__
 #define __ARSLEXIS_PHONE_CALL_HPP__
 
+#include <PalmOS.h>
+
 Err DialPhoneNumber(const char *phoneNumber);
 bool FCallPhoneNumber(const char *phoneNumber);
 
